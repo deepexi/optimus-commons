febc650455ee2d173c416c8604b7d18cdaf1407d
yunzi7758 <yunzi7758@DESKTOP-HBARHC9>
2018-07-11 17:27:52 +0800

    XmlUtil

---
+++ src/main/java/com/deepexi/util/XmlUtil.java
@@ -0,0 +1,45 @@
+package com.deepexi.util;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+
+import com.alibaba.fastjson.JSON;
+
+public class XmlUtil {
+
+	/**
+     * xml 转map
+     *
+     * @param xml
+     * @return
+     * @throws Exception
+     */
+    public Map<String, String> xml2Map(String xml) throws Exception {
+        Document document = DocumentHelper.parseText(xml);
+        //添加节点信息
+        Element rootElement = document.getRootElement();
+        Map<String, String> simMap = new TreeMap<>();
+        Iterator it = rootElement.elementIterator();
+        while (it.hasNext()) {
+            Element element = (Element) it.next();
+            simMap.put(element.getName(), element.getText());
+        }
+        return simMap;
+    }
+
+    /**
+     * xml 转bean
+     *
+     * @param xml
+     * @return
+     * @throws Exception
+     */
+    public <T> T xml2Bean(String xml, T t) throws Exception {
+        return (T) JSON.parseObject(JSON.toJSONString(xml2Map(xml)), t.getClass());
+    }
+}
