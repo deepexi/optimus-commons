8dce623eef2e916fe4aa6eb0eddb2f2a482baf6a
yunzi7758 <yunzi7758@DESKTOP-HBARHC9>
2018-07-13 10:09:03 +0800

    Base64Test

---
+++ src/test/java/com/deepexi/util/Base64Test.java
@@ -0,0 +1,19 @@
+package com.deepexi.util;
+
+import org.junit.Test;
+
+public class Base64Test {
+
+	@Test
+	public void encodeString() {
+		String encodeString = Base64.encodeString("1234145452");
+		System.out.println(encodeString);
+	}
+	
+	@Test
+	public void decodeString() {
+		
+		String decodeString = Base64.decodeString(Base64.encodeString("143244324335"));
+		System.out.println(decodeString);
+	}
+}
