8a35a926343daf8d7c5b8bc5f86c750aac6c0fe1
yunzi7758 <yunzi7758@DESKTOP-HBARHC9>
2018-07-13 11:02:50 +0800

    DateUtilsTest

--- src/test/java/com/deepexi/util/DateUtilsTest.java
+++ src/test/java/com/deepexi/util/DateUtilsTest.java
@@ -11,4 +11,28 @@ public class DateUtilsTest {
 		System.out.println(DateUtils.format(new Date()));
 		System.out.println(DateUtils.format(new Date(), DateUtils.DEFAULT_DATE_TIME_FORMAT));
 	}
+	
+	@Test
+	public void parse() {
+		System.out.println(DateUtils.parse(DateUtils.format(DateUtils.now())));
+		System.out.println(DateUtils.parse(DateUtils.format(DateUtils.now(), DateUtils.DEFAULT_DATE_TIME_FORMAT)));
+		System.out.println(DateUtils.parse(DateUtils.format(DateUtils.now(), DateUtils.DEFAULT_DATE_TIME_FORMAT)
+				, DateUtils.DEFAULT_DATE_TIME_FORMAT));
+	}
+	
+	@Test
+	public void otherMethod() {
+		System.out.println(DateUtils.now());
+		
+		System.out.println(DateUtils.nextYear(DateUtils.now(), 1));
+		System.out.println(DateUtils.nextYear(DateUtils.now(), -2));
+		
+		System.out.println(DateUtils.getDayByInterval(DateUtils.now(), 1));
+		System.out.println(DateUtils.getDayByInterval(DateUtils.now(), -3));
+		
+		System.out.println(DateUtils.getWeekStr(DateUtils.format(new Date())));
+		
+		
+		System.out.println(DateUtils.getCurrenDate());
+	}
 }
