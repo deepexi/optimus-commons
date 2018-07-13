d730c2896a4e43aaf8ef4ade2effe911102be4ef
yunzi7758 <yunzi7758@DESKTOP-HBARHC9>
2018-07-13 10:44:16 +0800

    DateUtilsTest

--- src/test/java/com/deepexi/util/DateUtilsTest.java
+++ src/test/java/com/deepexi/util/DateUtilsTest.java
@@ -1,6 +1,13 @@
 package com.deepexi.util;
 
+import java.util.Date;
+
+import org.junit.Test;
+
 public class DateUtilsTest {
 
-	
+	@Test
+	public void format() {
+		System.out.println(DateUtils.format(new Date()));
+	}
 }
