0128711ab59ebac1aa8807130ed43a461bd9d851
yunzi7758 <yunzi7758@yunzi7758>
2018-07-19 14:34:09 +0800

    字符串首字母改大小写

--- src/main/java/com/deepexi/util/StringUtil.java
+++ src/main/java/com/deepexi/util/StringUtil.java
@@ -189,8 +189,38 @@ public class StringUtil {
 	    return !isEmpty(cs);
 	  }
 	  
+	  
+	  /**
+	   * 首字母转小写
+	   * @param s
+	   * @return
+	   */
+	  public static String toLowerCaseFirstOne(String s){
+	    if(Character.isLowerCase(s.charAt(0))) {
+			return s;
+		} else {
+			return (new StringBuilder()).append(Character.toLowerCase(s.charAt(0))).append(s.substring(1)).toString();
+		}
+	  }
+
+
+	  /**
+	   * 首字母转大写
+	   * @param s
+	   * @return
+	   */
+	  public static String toUpperCaseFirstOne(String s){
+	    if(Character.isUpperCase(s.charAt(0))) {
+			return s;
+		} else {
+			return (new StringBuilder()).append(Character.toUpperCase(s.charAt(0))).append(s.substring(1)).toString();
+		}
+	  }
+	  
 	public static void main(String[] args) {
 		try{
+			System.out.println(toLowerCaseFirstOne("ADFSD"));
+			System.out.println(toUpperCaseFirstOne("dadfad"));
 			System.out.println(getRandomString(12));
 		}catch(Exception e){
 			System.out.println(parseFromException(e));
